b7fb49e9dbc77639bd836763aaa0d18df426ea8f
Paweł Wangryn <pwangryn@op.pl>
2015-12-08 10:56:12 +0100

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-ifneq ($(filter d315,$(TARGET_DEVICE)),)
+ifneq ($(filter f70n,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
