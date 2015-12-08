2edc19687405b1c16e556ad0ba364b2cc95c973b
Paweł Wangryn <pwangryn@op.pl>
2015-12-08 11:03:55 +0100

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -6,7 +6,9 @@ TARGET_SCREEN_WIDTH := 480
 TARGET_SCREEN_HEIGHT := 800
 
 # Release name
-PRODUCT_RELEASE_NAME := LG_F70n
-PRODUCT_NAME := cm_d315
+PRODUCT_RELEASE_NAME := LG F70
+PRODUCT_NAME := cm_f70n
+PRODUCT_MANUFACTURER := lge
+PRODUCT_MODEL := f70n
 
-$(call inherit-product, device/lge/d315/full_d315.mk)
+$(call inherit-product, device/lge/f70n/full_f70n.mk)
