433a567ffe6c1af257b38fda76531c802dbf8157
Paweł Wangryn <pwangryn@op.pl>
2015-12-08 11:01:02 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,87 +24,83 @@
 USE_CAMERA_STUB := false
 TARGET_NO_BOOTLOADER := true
 
-TARGET_SPECIFIC_HEADER_PATH += device/lge/d315/include
+TARGET_SPECIFIC_HEADER_PATH += device/lge/f70n/include
+
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := device/lge/f70n
 
 # Vendor Init
-#TARGET_UNIFIED_DEVICE := true
-#TARGET_INIT_VENDOR_LIB := libinit_msm
-#TARGET_LIBINIT_DEFINES_FILE := device/lge/d315/init/init_d315.c
+TARGET_UNIFIED_DEVICE := true
+TARGET_INIT_VENDOR_LIB := libinit_msm
+TARGET_LIBINIT_DEFINES_FILE := device/lge/f70n/init/init_f70n.c
+TARGET_OTA_ASSERT_DEVICE := f70n
+TARGET_INCREMENTAL_OTA_VERBATIM_FILES := system/priv-app/OneTimeInitializer/OneTimeInitializer.apk,system/app/Provision/Provision.apk
+ART_USE_HSPACE_COMPACT := true
+MALLOC_IMPL := dlmalloc
 
 # Platform
-TARGET_ARCH := arm
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno305
 TARGET_BOARD_PLATFORM := msm8226
-TARGET_CPU_VARIANT := krait
-TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
-TARGET_USE_KINGFISHER_OPTIMIZATION := true
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno305
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-ARCH_ARM_HAVE_TLS_REGISTER := true
-TARGET_BOOTLOADER_BOARD_NAME := d315
+TARGET_CPU_MEMCPY_BASE_OPT_DISABLE := true
+TARGET_CPU_VARIANT := krait
 
-TARGET_SCREEN_HEIGHT := 800
-TARGET_SCREEN_WIDTH := 480
+TARGET_BOOTLOADER_BOARD_NAME := f70n
 
 # Kernel image
 BOARD_KERNEL_SEPARATED_DT := true
-BOARD_CUSTOM_BOOTIMG_MK := device/lge/d315/mkbootimg.mk
-TARGET_KERNEL_SOURCE := kernel/lge/d315
-TARGET_KERNEL_CONFIG := f70n_global_com_defconfig
-# TARGET_KERNEL_CONFIG := f70n_global_com_defconfig
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 user_debug=31 msm_rtb.filter=0x37 androidboot.hardware=f70n
+BOARD_CUSTOM_BOOTIMG_MK := device/lge/f70n/mkbootimg.mk
+TARGET_KERNEL_SOURCE := kernel/lge/msm8226
+TARGET_KERNEL_CONFIG := w7ds_cyanogenmod_defconfig
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 user_debug=31 msm_rtb.filter=0x37 androidboot.hardware=w7 androidboot.selinux=enforcing
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_OFFSET := 0x00008000
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x01e00000
-#TARGET_PREBUILT_KERNEL := device/lge/d315/kernel
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+
+# Offmode Charging
+COMMON_GLOBAL_CFLAGS += \
+    -DBOARD_CHARGING_CMDLINE_NAME='"androidboot.mode"' \
+    -DBOARD_CHARGING_CMDLINE_VALUE='"chargerlogo"'
 
 # Global flags
-COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DLG_CAMERA_HARDWARE
+COMMON_GLOBAL_CFLAGS +=  -DLG_CAMERA_HARDWARE
 
-# QCOM BSP
-TARGET_USES_QCOM_BSP := true
-COMMON_GLOBAL_CFLAGS += -DQCOM_BSP
+# QCOM hardware
+BOARD_USES_QCOM_HARDWARE := true
+
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
 
 # Audio
-AUDIO_FEATURE_DISABLED_FM := false
-AUDIO_FEATURE_DISABLED_SSR := true
-BOARD_HAVE_QCOM_FM := true
-AUDIO_FEATURE_DISABLED_ANC_HEADSET := true
-AUDIO_FEATURE_DISABLED_DS1_DOLBY_DDP := true
+AUDIO_FEATURE_ENABLED_FM := true
+AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS := true
 BOARD_USES_ALSA_AUDIO := true
-BOARD_USES_FLUENCE_INCALL := true
-BOARD_USES_SEPERATED_AUDIO_INPUT := true
-BOARD_USES_SEPERATED_VOICE_SPEAKER := true
-TARGET_QCOM_AUDIO_VARIANT := caf
-TARGET_USES_QCOM_COMPRESSED_AUDIO := true
-TARGET_QCOM_MEDIA_VARIANT := caf-new
+TARGET_QCOM_NO_FM_FIRMWARE := true
 
-# GPS
-TARGET_NO_RPC := true
-
-# Graphics
-BOARD_EGL_CFG := device/lge/d315/prebuilt/egl.cfg
-TARGET_DISPLAY_USE_RETIRE_FENCE := true
-TARGET_QCOM_DISPLAY_VARIANT := caf-new
-USE_OPENGL_RENDERER := true
+# Display
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 TARGET_USES_C2D_COMPOSITION := true
 TARGET_USES_ION := true
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
-OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
-
-# Use qcom power hal
-TARGET_POWERHAL_VARIANT := qcom
-TARGET_USES_CPU_BOOST_HINT := true
-
-TARGET_HW_DISK_ENCRYPTION := true
+USE_OPENGL_RENDERER := true
 
 # Hardware tunables framework
-BOARD_HARDWARE_CLASS := device/lge/d315/cmhw/
+BOARD_HARDWARE_CLASS := device/lge/f70n/cmhw/
+
+# RIL
+TARGET_RIL_VARIANT := caf
+COMMON_GLOBAL_CFLAGS += -DUSE_RIL_VERSION_10
+COMMON_GLOBAL_CPPFLAGS += -DUSE_RIL_VERSION_10
+#QC_PROP_ROOT := vendor/qcom/proprietary
+PROTOBUF_SUPPORTED := true
+TARGET_NO_RPC := true
 
 # Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/d315/bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/w7/bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_QCOM := true
 BLUETOOTH_HCI_USE_MCT := true
@@ -122,52 +118,35 @@ TARGET_USES_QCOM_WCNSS_QMI := true
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_AP := "ap"
 
-# Enable WEBGL in WebKit
-ENABLE_WEBGL := true
-TARGET_FORCE_CPU_UPLOAD := true
-
-# QCOM hardware
-BOARD_USES_QCOM_HARDWARE := true
-
-# QCOM enhanced A/V
-TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
-
 # Camera
 USE_DEVICE_SPECIFIC_CAMERA := true
-TARGET_DISPLAY_INSECURE_MM_HEAP := true
-
-# Number of supplementary service groups allowed by init
-TARGET_NR_SVC_SUPP_GIDS := 28
-TARGET_BOOTANIMATION_PRELOAD := true
-TARGET_BOOTANIMATION_TEXTURE_CACHE := false
-
-# Time services
-BOARD_USES_QC_TIME_SERVICES := true
 
 # Storage
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
 BOARD_VOLD_MAX_PARTITIONS := 40
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01600000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01600000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1560281088
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00E00000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00E00000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 754974720
 BOARD_FLASH_BLOCK_SIZE := 131072
-BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 
 # TWRP Recovery
-# TARGET_PREBUILT_RECOVERY_KERNEL := device/lge/d315/rootdir/kernel
-TARGET_RECOVERY_FSTAB := device/lge/d315/rootdir/recovery.fstab
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_FSTAB := device/lge/w7/rootdir/fstab.f70n
 RECOVERY_FSTAB_VERSION := 2
-DEVICE_RESOLUTION := 480x800
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+DEVICE_RESOLUTION := 540x960
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_EXTERNAL_STORAGE_PATH := "/sdcard"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "sdcard"
+TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_FLASH_FROM_STORAGE := true
 BOARD_UMS_LUNFILE := /sys/devices/platform/msm_hsusb/gadget/lun0/file
-RECOVERY_SDCARD_ON_DATA := true
-TW_INTERNAL_STORAGE_PATH := "/sdcard"
-TW_INTERNAL_STORAGE_MOUNT_POINT := "sdcard"
-TW_EXTERNAL_STORAGE_PATH := "/external_sd"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
-TW_DEFAULT_EXTERNAL_STORAGE := true
+TW_INTERNAL_STORAGE_PATH := "/data/media"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 BOARD_HAS_NO_SELECT_BUTTON := true
@@ -175,68 +154,26 @@ BOARD_RECOVERY_SWIPE := true
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
 BOARD_SUPPRESS_EMMC_WIPE := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
+RECOVERY_SDCARD_ON_DATA := true
 HAVE_SELINUX := true
 BOARD_HAS_NO_MISC_PARTITION := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
-TW_SCREEN_BLANK_ON_BOOT := true
-TW_NO_REBOOT_BOOTLOADER := true
-TW_NO_USB_STORAGE := true
 
-PRODUCT_COPY_FILES += device/lge/d315/rootdir/twrp.fstab:recovery/root/etc/twrp.fstab
-PRODUCT_COPY_FILES += device/lge/d315/rootdir/extra.fstab:recovery/root/etc/extra.fstab
+# Enable Minikin text layout engine (will be the default soon)
+USE_MINIKIN := true
+
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT := true
+
+# Use HW crypto for ODE
+TARGET_HW_DISK_ENCRYPTION := true
 
 # Nfc
-BOARD_NFC_HAL_SUFFIX := d315
 BOARD_NFC_CHIPSET := pn547
 
-# SELinux
+# SELinux policies
+# qcom sepolicy
+include device/qcom/sepolicy/sepolicy.mk
+
 BOARD_SEPOLICY_DIRS += \
-	device/lge/d315/sepolicy
-
-BOARD_SEPOLICY_UNION += \
-	adbd.te \
-	app.te \
-	bluetooth_loader.te \
-	bridge.te \
-	camera.te \
-	device.te \
-	dhcp.te \
-	dnsmasq.te \
-	domain.te \
-	drmserver.te \
-	file_contexts \
-	file.te \
-	hostapd.te \
-	init_shell.te \
-	init.te \
-	libqc-opt.te \
-	mediaserver.te \
-	mpdecision.te \
-	netd.te \
-	netmgrd.te \
-	nfc.te \
-	property_contexts \
-	property.te \
-	qcom.te \
-	qmux.te \
-	radio.te \
-	rild.te \
-	rmt.te \
-	sdcard_internal.te \
-	sdcardd.te \
-	sensors.te \
-	shell.te \
-	surfaceflinger.te \
-	system.te \
-	tee.te \
-	te_macros \
-	thermald.te \
-	ueventd.te \
-	vold.te \
-	wpa_supplicant.te \
-	zygote.te
-
-ifneq ($(TARGET_BUILD_VARIANT),user)
-	BOARD_SEPOLICY_UNION += su.te
-endif
-
+        device/lge/f70n/sepolicy
